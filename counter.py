de8084c90d2f5a5809ded007dedb19e555686c6b
Prakrit Maan <prakrit.078@gmail.com>
2022-03-20 14:51:15 +0530

    counter py
    ******************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************

---
+++ counter.py
@@ -0,0 +1,14 @@
+#variable declaration
+#putin ahhahahahhahahahhah
+putin = input ("HI! what are u?")
+character_count = 0 
+word_count = 1
+#word counter 
+for i in (putin) : 
+    character_count +=1
+    if i == (" ") : 
+        word_count +=1
+#final print 
+print (character_count)
+print (word_count)
+# this is simple 
